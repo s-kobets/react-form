f7a309b0c03c6995434bff624efc81de5f7da0cd
s-kobets <s-kobets617@yandex.ru>
2017-05-04 09:42:45 +0300

    add input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './store'
+import Forms from './component/'
 
 class App extends Component {
   render() {    
@@ -16,6 +17,7 @@ class App extends Component {
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
 
+          <Forms />
         </div>
       </Provider>
     );
